cdfb890814cfdebbac459acec6c2f03367940c1a
Ankit Kumar <ankitkumar2592004@gmail.com>
2025-12-29 00:18:53 +0000

    useat chat bot logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,20 @@
-import { type LucideIcon, MessageCircle } from 'lucide-react';
-
-export type Icon = LucideIcon;
-
-export const Icons = {
-  logo: MessageCircle,
-};
+export const ManoIndiaLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    {...props}
+    >
+    <path
+      fill="#E6B325"
+      d="M50 15C52.7614 15 55 17.2386 55 20C55 22.7614 52.7614 25 50 25C47.2386 25 45 22.7614 45 20C45 17.2386 47.2386 15 50 15Z"
+    />
+    <path
+      fill="#F0C642"
+      d="M50 26C55.5228 26 60 30.4772 60 36V42H40V36C40 30.4772 44.4772 26 50 26Z"
+    />
+    <path
+      fill="#C48A27"
+      d="M10 60L30 60L50 80L70 60L90 60L75 45L50 70L25 45L10 60Z"
+    />
+  </svg>
+);
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
+import { ManoIndiaLogo } from '../icons';
 
 export default function Header() {
   return (
@@ -8,7 +8,7 @@ export default function Header() {
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Icons.logo className="h-6 w-6 text-primary" />
+            <ManoIndiaLogo className="h-8 w-8" />
             <span className="font-bold">
               Mano India
             </span>
